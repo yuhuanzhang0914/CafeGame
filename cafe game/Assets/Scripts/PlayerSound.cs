385500d770826314cc77d68ac46c53c51ca122e7
ZhuKaiyin <3312067664@qq.com>
2025-11-24 03:39:32 +0800

    Player footsteps

---
+++ cafe game/Assets/Scripts/PlayerSound.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    private Player player;
+    private float stepSoundRate = 0.1f;
+    private float stepSoundTimer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        stepSoundTimer += Time.deltaTime;
+        if(stepSoundTimer>stepSoundRate)
+        {
+            stepSoundTimer = 0;
+            if (player.IsWalking)
+            {
+                float volume = 1;
+                SoundManager.Instance.PlayStepSound(volume);
+            }
+        }
+    }
+}
--- cafe game/Assets/Scripts/SoundManager.cs
+++ cafe game/Assets/Scripts/SoundManager.cs
@@ -5,8 +5,12 @@ using System;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance { get; private set; }
     [SerializeField] private AudioClipRefsS0 audioClipRefsSO;
-
+    private void Awake()
+    {
+        Instance = this;
+    }
     private void Start()
     {
         OrderManager.Instance.OnRecipeSuccess += OrderManager_OnRecipeSuccess;
@@ -42,23 +46,26 @@ public class SoundManager : MonoBehaviour
 
     private void OrderManager_OnRecipeSuccess(object sender, EventArgs e)
     {
-        // 成功时播放成功音效
+       
         PlaySound(audioClipRefsSO.deliverySuccess, Camera.main.transform.position);
     }
 
     private void OrderManager_OnRecipeFailed(object sender, EventArgs e)
     {
-        // 失败时播放失败音效
+       
         PlaySound(audioClipRefsSO.deliveryFail, Camera.main.transform.position);
     }
-
-    // 只传数组的简写，在主摄像机位置播放
+    public void PlayStepSound(float volume=1f)
+    {
+        PlaySound(audioClipRefsSO.footstep,volume);
+    }
+    
     private void PlaySound(AudioClip[] clips, float volume = 1.0f)
     {
         PlaySound(clips, Camera.main.transform.position, volume);
     }
 
-    // 真正播放函数
+    
     private void PlaySound(AudioClip[] clips, Vector3 position, float volume = 1.0f)
     {
         int index = UnityEngine.Random.Range(0, clips.Length);
